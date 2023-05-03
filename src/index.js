d35356c33efacc27ac3e40e3fe2ec8271d1e82ec
HlobisileLukhele <n.thwala03@gmail.com>
2023-05-03 08:09:52 -0700

    creating function to display the weather forecast

--- src/index.js
+++ src/index.js
@@ -61,4 +61,38 @@ function displayForecast(response) {
     console.log(apiUrl);
     axios.get(apiUrl).then(displayForecast);
   }
+  //Display the weather forecast 
+
+  function displayWeather(response) {
+    //Checking how to find the element in the API response
+    //console.log(response.data);
+  
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+  
+    let dateElement = document.querySelector("#date");
+  
+    let iconElement = document.querySelector("#weather-icon");
+  
+    celsiusTemperture = response.data.main.temp;
+  
+    temperatureElement.innerHTML = Math.round(celsiusTemperture);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    date.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+  
+    //console.log(response.data);
+  
+    getForecast(response.data.coord);
+  }
   
