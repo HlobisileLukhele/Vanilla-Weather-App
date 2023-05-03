0ab0df41a17a85cbf40fbe94df47967aecea5d77
HlobisileLukhele <n.thwala03@gmail.com>
2023-05-03 08:03:09 -0700

    getting forecast using coordinated

--- src/index.js
+++ src/index.js
@@ -53,4 +53,12 @@ function displayForecast(response) {
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
   }
+  // getting forecast using coordinated 
+  function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "34ae1065362d42545661451bda2b8a1f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+  }
   
